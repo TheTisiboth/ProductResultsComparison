4fd7cc0146a507ef0a63126eedf71e3c86c02471
Jan <thetisiboth@gmail.com>
2021-02-15 18:10:02 +0100

    Try to optimise scrolling

--- src/main/java/org/openlca/display/ProductDisplay.java
+++ src/main/java/org/openlca/display/ProductDisplay.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Path;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
@@ -13,6 +16,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.ScrollBar;
@@ -66,10 +70,16 @@ public class ProductDisplay {
 		ScrollBar vBar = canvas.getVerticalBar();
 		vBar.setMaximum(theoreticalScreenHeight);
 		vBar.setMinimum(0);
-		
+
 		addScrollListener(canvas, vBar);
 		addResizeEvent(composite, canvas, vBar);
-		addPaintListener(composite, canvas);
+
+		Image cache = new Image(Display.getCurrent(), 1904, theoreticalScreenHeight);
+		GC gc = new GC(cache);
+		paint(gc, composite);
+		ImageData data = cache.getImageData();
+		addPaintListener(composite, canvas, gc, cache);
+
 //		canvas.redraw();
 	}
 
@@ -119,95 +129,84 @@ public class ProductDisplay {
 		});
 	}
 
+	private void paint(GC gc, Composite composite) {
+		System.out.println("Paint");
+		screenSize = composite.getSize(); // Responsive behavior
+		double maxProductWidth = screenSize.x * 0.8; // 80% of the screen width
+		Point rectEdge = new Point(origin.x + xMargin, origin.y + xMargin); // Start point of the first product
+		// rectangle
+		for (int productIndex = 0; productIndex < products.size(); productIndex++) {
+			Product p = products.get(productIndex);
+			final int productResultsAmount = p.getList().size();
+			int productWidth = (int) ((productResultsAmount / maxProductResultsAmout) * maxProductWidth);
+			int gap = (int) (productWidth / productResultsAmount);
+
+			// Draw a rectangle for each product
+			gc.drawRoundRectangle(rectEdge.x, rectEdge.y, (int) productWidth, productHeight, 30, 30);
+			Point prevSubRectEdge = rectEdge; // Coordinate of each result rectangle
+			for (int resultIndex = 0; resultIndex < productResultsAmount; resultIndex++) {
+				Result result1 = p.getResult(resultIndex);
+				prevSubRectEdge = handleResult(gc, productIndex, productResultsAmount, prevSubRectEdge, resultIndex,
+						result1, gap, rectEdge);
+			}
+			rectEdge.y += 300;
+		}
+		drawLinks(gc);
+	}
+
+	private Point handleResult(GC gc, int productIndex, final int productResultsAmount, Point prevSubRectEdge,
+			int resultIndex, Result result, int gap, Point rectEdge) {
+		// Draw a separator line between the current result, and the next one
+		Point sepStart = new Point(rectEdge.x + (resultIndex + 1) * (int) gap, rectEdge.y);
+		Point sepEnd = new Point(sepStart.x, rectEdge.y + productHeight);
+		if (resultIndex != productResultsAmount - 1) {
+			gc.drawLine(sepStart.x, sepStart.y, sepEnd.x, sepEnd.y);
+		}
+		if (config.displayResultValue) {
+			Point textPos = new Point(prevSubRectEdge.x + 10, (sepEnd.y + sepStart.y) / 2 - 10);
+			gc.drawText(result.toString(), textPos.x, textPos.y);
+		}
+		result.setStartPoint((prevSubRectEdge.x + sepEnd.x) / 2, sepEnd.y);
+		result.setEndPoint((prevSubRectEdge.x + sepEnd.x) / 2, sepStart.y);
+		if (productIndex + 1 < products.size()) { // If there is a next product
+			Product p2 = products.get(productIndex + 1);
+			// We searh the first matching result
+			Optional<Result> result2 = p2.getList().stream().filter(r2 -> result.equals(r2)).findFirst();
+			if (result2.isPresent()) {
+				result.setTargetProductResult(result2.get());
+			}
+		}
+		return sepStart;
+	}
+
+	private void drawLinks(GC gc) {
+		for (Product product : products) {
+			for (Result result : product.getList()) {
+				Point p1 = result.getStartPoint();
+				Result result2 = result.getTargetProductResult();
+				if (result2 != null) {
+					Point p2 = result2.getEndPoint();
+					drawBezierCurve(gc, p1, p2);
+				}
+			}
+		}
+	}
+
 	/**
 	 * Add a paint listener to the canvas
 	 * 
 	 * @param composite Parent composent of the canvas
 	 * @param canvas    A Canvas component
 	 */
-	private void addPaintListener(Composite composite, Canvas canvas) {
+	private void addPaintListener(Composite composite, Canvas canvas, GC gc, Image cache) {
 		canvas.addPaintListener(new PaintListener() {
 			public void paintControl(PaintEvent e) {
-				System.out.println("Paint event");
 				screenSize = composite.getSize(); // Responsive behavior
-				double maxProductWidth = screenSize.x * 0.8; // 80% of the screen width
-				Point rectEdge = new Point(origin.x + xMargin, origin.y + xMargin); // Start point of the first product
-				// rectangle
-				for (int productIndex = 0; productIndex < products.size(); productIndex++) {
-					Product p = products.get(productIndex);
-					final int productResultsAmount = p.getList().size();
-					int productWidth = (int) ((productResultsAmount / maxProductResultsAmout) * maxProductWidth);
-					int gap = (int) (productWidth / productResultsAmount);
-
-					// Draw a rectangle for each product
-					e.gc.drawRoundRectangle(rectEdge.x, rectEdge.y, (int) productWidth, productHeight, 30, 30);
-					Point prevSubRectEdge = rectEdge; // Coordinate of each result rectangle
-					for (int resultIndex = 0; resultIndex < productResultsAmount; resultIndex++) {
-						Result result1 = p.getResult(resultIndex);
-						prevSubRectEdge = handleResult(e, productIndex, productResultsAmount, prevSubRectEdge,
-								resultIndex, result1, gap, rectEdge);
-					}
-					rectEdge.y += 300;
-				}
-				drawLinks(e);
+				System.out.println(screenSize);
+				ImageData data = cache.getImageData();
+				gc.drawImage(cache, 0, origin.y, screenSize.x, screenSize.y, 0, 0, screenSize.x, screenSize.y);
 			}
 
-			/**
-			 * Handle the current result. Draw a separation line between it and the next
-			 * result, display its value, and find a matching result in the next product
-			 * 
-			 * @param e                    The Paint Event
-			 * @param productIndex         Index of the current product
-			 * @param productResultsAmount Amount of results for the current product
-			 * @param prevSubRectEdge      Separation with the previous result
-			 * @param resultIndex          The index result
-			 * @param result               THe current result
-			 * @param gap                  Gap between 2 products
-			 * @param rectEdge             Position of product rectangle
-			 * @return
-			 */
-			private Point handleResult(PaintEvent e, int productIndex, final int productResultsAmount,
-					Point prevSubRectEdge, int resultIndex, Result result, int gap, Point rectEdge) {
-				// Draw a separator line between the current result, and the next one
-				Point sepStart = new Point(rectEdge.x + (resultIndex + 1) * (int) gap, rectEdge.y);
-				Point sepEnd = new Point(sepStart.x, rectEdge.y + productHeight);
-				if (resultIndex != productResultsAmount - 1) {
-					e.gc.drawLine(sepStart.x, sepStart.y, sepEnd.x, sepEnd.y);
-				}
-				if (config.displayResultValue) {
-					Point textPos = new Point(prevSubRectEdge.x + 10, (sepEnd.y + sepStart.y) / 2 - 10);
-					e.gc.drawText(result.toString(), textPos.x, textPos.y);
-				}
-				result.setStartPoint((prevSubRectEdge.x + sepEnd.x) / 2, sepEnd.y);
-				result.setEndPoint((prevSubRectEdge.x + sepEnd.x) / 2, sepStart.y);
-				if (productIndex + 1 < products.size()) { // If there is a next product
-					Product p2 = products.get(productIndex + 1);
-					// We searh the first matching result
-					Optional<Result> result2 = p2.getList().stream().filter(r2 -> result.equals(r2)).findFirst();
-					if (result2.isPresent()) {
-						result.setTargetProductResult(result2.get());
-					}
-				}
-				return sepStart;
-			}
-
-			/**
-			 * Draw the links between each matching results
-			 * 
-			 * @param e
-			 */
-			private void drawLinks(PaintEvent e) {
-				for (Product product : products) {
-					for (Result result : product.getList()) {
-						Point p1 = result.getStartPoint();
-						Result result2 = result.getTargetProductResult();
-						if (result2 != null) {
-							Point p2 = result2.getEndPoint();
-							drawBezierCurve(e, p1, p2);
-						}
-					}
-				}
-			}
 		});
 	}
 
@@ -218,13 +217,13 @@ public class ProductDisplay {
 	 * @param start The starting point
 	 * @param end   The ending point
 	 */
-	private void drawBezierCurve(PaintEvent e, Point start, Point end) {
-		Path p = new Path(e.gc.getDevice());
+	private void drawBezierCurve(GC gc, Point start, Point end) {
+		Path p = new Path(gc.getDevice());
 		p.moveTo(start.x, start.y);
 		int offset = 100;
 		Point ctrlPoint1 = new Point(start.x + offset, start.y + offset);
 		Point ctrlPoint2 = new Point(end.x - offset, end.y - offset);
 		p.cubicTo(ctrlPoint1.x, ctrlPoint1.y, ctrlPoint2.x, ctrlPoint2.y, end.x, end.y);
-		e.gc.drawPath(p);
+		gc.drawPath(p);
 	}
 }
